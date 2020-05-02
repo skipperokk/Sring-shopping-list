35201c797fcee8ed51062507dc1b9c02441cb383
skipperokk <Kh9yK$Rf>
2020-05-02 16:21:22 +0300

    Initial commit

---
+++ src/main/java/ru/springtask/persist/ShoppingItem.java
@@ -0,0 +1,30 @@
+package ru.springtask.persist;
+
+import javax.persistence.*;
+
+@Entity
+@Table (name ="shopping_items")
+public class ShoppingItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/ru/springtask/persist/ShoppingItemRepository.java
@@ -0,0 +1,8 @@
+package ru.springtask.persist;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ShoppingItemRepository extends JpaRepository<ShoppingItem, Long> {
+}
